d3ab43ab8e39391f351f65bae33bb59665cd717b
Zchun <z.c.chun2003@gmail.com>
2022-08-07 20:19:50 -0500

    readme

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/Connection");
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'uber super secret',
